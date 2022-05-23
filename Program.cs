9bec90bf6b711c99cbc247c3f541a1a010db509a
TuhakarainaB <Bruce.Tuhakaraina@toiohomai.ac.nz>
2022-05-24 09:26:36 +1200

    Created Start function and updated display text with tabs.

--- Program.cs
+++ Program.cs
@@ -8,24 +8,31 @@ namespace eShop_Levi_Lewis
 {
     internal class Program
     {
-        public static string UserName = "";
-        
+       public static string UserName = "";
+
+
+        static void Start()
+        {
+            Console.WriteLine("\tWelcome to our eShop, please enter your name\n");
+            UserName = Console.ReadLine();
+            Console.WriteLine("\tWelcome " + UserName + " Please select an option from the menu below");
+            Console.WriteLine("\t[1] Check Balance \n \t[2] Clothing Menu \n \t[3] Technoloy Menu\n");
+            MenuOne();
+        }
+
         static void Main()
         {
-            Console.WriteLine("Welcome to our eShop, please enter your name");
-            UserName = Console.ReadLine();
-            Console.WriteLine("Welcome " + UserName + " Please select an option from the menu below");
-            Console.WriteLine("{1} Check Balance \n [2] Clothing Menu \n [3] Technoloy Menu");
-            MenuOne();
+            Start();
         }// end of Main
 
         static void MenuOne()
         {
-            Console.WriteLine("This is the clothing menu/n");
-            
-
+            Console.WriteLine("\t********************************\n");
+            Console.WriteLine("[This is the clothing menu]\n");
 
         }//end of MenuOne
+
+    }
 
     }//end of internal class Program
 }// end of namespace
